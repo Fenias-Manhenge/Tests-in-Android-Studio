9c326685c88cb54179f7b818ced7f02c77f91fdb
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2023-03-28 23:16:58 +0200

    Gallery permission!
    Adding gallery permission and rationale Dialog!

--- app/src/main/java/com/example/tests/ImplicitIntent.kt
+++ app/src/main/java/com/example/tests/ImplicitIntent.kt
@@ -1,10 +1,13 @@
 package com.example.tests
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.tests.databinding.ImplicitIntentBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ImplicitIntent : AppCompatActivity() {
 
@@ -15,16 +18,55 @@ class ImplicitIntent : AppCompatActivity() {
         binding = ImplicitIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val openGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if (it.resultCode == RESULT_OK && it.data != null)
-                binding.ivPhoto.setImageURI(it.data?.data)
-        }
-
         binding.btnAddImage.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT).also { intent ->
-                intent.type = "image/*"
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+                rationaleDialog(
+                    "XIMIX requires External Storage access",
+                    "We need your External Storage permission to access the Gallery"
+                )
+            }else {
+                externalStoragePermission.launch(android.Manifest.permission.CAMERA)
             }
-            openGallery.launch(intent)
         }
+    }
+
+    private val openGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == RESULT_OK && it.data != null)
+            binding.ivPhoto.setImageURI(it.data?.data)
+    }
+
+    private val intentGalleryContent = Intent(Intent.ACTION_GET_CONTENT).also {
+        it.type = "image/*"
+    }
+
+    private val externalStoragePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGrated->
+        if (isGrated) {
+            openGallery.launch(intentGalleryContent)
+        }else {
+            Snackbar.make(binding.root, "Permission denied for Camera", Snackbar.LENGTH_SHORT)
+                .setAction("Allow") {
+                    rationaleDialog(
+                        "XIMIX requires External Storage access",
+                        "We need your External Storage permission to access the Gallery"
+                    )
+                }
+                .setActionTextColor(ContextCompat.getColor(this, R.color.md_theme_dark_surfaceTint))
+                .show()
+        }
+    }
+
+    private fun rationaleDialog(title: String, message: String){
+        AlertDialog.Builder(this).apply {
+            setTitle(title)
+            setMessage(message)
+            setIcon(R.drawable.storage_hdd)
+            setCancelable(false)
+
+            setPositiveButton("Ok"){ dialog, _->
+                dialog.dismiss()
+                externalStoragePermission.launch(android.Manifest.permission.CAMERA)
+            }
+        }.show()
     }
 }
